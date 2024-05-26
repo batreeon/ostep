73606c9ed1589c7aad4037aef529f0e5176b1eb7
batreeon <1340551309@qq.com>
2024-05-26 18:06:03 +0800

    六种不同的exec()函数

---
+++ homework/5/4/1_execl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        if(execl("/bin/ls", "ls", NULL) == -1) {
+            printf("execl failed!\n");
+        }
+    }
+    return 0;
+}
---
+++ homework/5/4/6_execvpe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        char *args[] = {"ls", NULL};
+        if(execvpe("ls", args) == -1) {
+            printf("execl failed!\n");
+        }
+    }
+    return 0;
+}
